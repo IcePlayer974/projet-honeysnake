22564facdda9b72321d7b9c3b83b3de6824e73db
Etil <81570777+etil2jz@users.noreply.github.com>
2021-09-24 09:20:30 +0200

    un peu plus de taff sur le css

--- js/jeu.js
+++ js/jeu.js
@@ -1,15 +1,15 @@
 // ça charge le javascript uniquement quand la page a entièrement chargé
 window.onload=function() {
 	var canvas = document.getElementById('zone'); // on récupère le canvas d'id zone dans html
-	var context = canvas.getContext('2d'); // c'est expliqué là Julien : https://developer.mozilla.org/fr/docs/Web/API/Canvas_API/Tutorial/Basic_usage
-	var interval = setInterval(jeu, 10); // vérification toutes les 10ms
-	document.addEventListener("keydown", clavier); // on écoute les events du clavier
+	var ctx = canvas.getContext('2d'); // c'est expliqué là Julien : https://developer.mozilla.org/fr/docs/Web/API/Canvas_API/Tutorial/Basic_usage
+	var intervalID = setInterval(jeu, 10); // vérification toutes les 10ms
+	document.addEventListener("keydown", keyboard); // on écoute les events du clavier
 }
 function jeu() {
 	// le plus gros du taff mdr
 }
-function clavier(action) {
-	switch(action.keyCode) {
+function keyboard(evt) {
+	switch(evt.keyCode) {
 	case 37: // à faire - tourner à gauche
 	break;
 	case 38: // à faire - monter
