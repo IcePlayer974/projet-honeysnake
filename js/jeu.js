b5b974500d65eb487ca12ef60d94018d09248d8d
Etil <81570777+etil2jz@users.noreply.github.com>
2021-09-24 10:54:37 +0200

    on a un pixel au centre, grosse étape de faite

--- js/jeu.js
+++ js/jeu.js
@@ -1,15 +1,23 @@
-// ça charge le javascript uniquement quand la page a entièrement chargé
+var canvas = document.getElementById('canvas'); // on récupère le canvas d'id zone dans html
+var contexte = canvas.getContext('2d'); // c'est expliqué là Julien : https://developer.mozilla.org/fr/docs/Web/API/Canvas_API/Tutorial/Basic_usage
+var largeur = hauteur = 25;
+var x = Math.trunc(canvas.width / 2);
+var y = Math.trunc(canvas.height / 2);
+
+// ça charge ce bout de code uniquement quand la page a entièrement chargé
 window.onload=function() {
-	var canvas = document.getElementById('zone'); // on récupère le canvas d'id zone dans html
-	var ctx = canvas.getContext('2d'); // c'est expliqué là Julien : https://developer.mozilla.org/fr/docs/Web/API/Canvas_API/Tutorial/Basic_usage
-	var intervalID = setInterval(jeu, 10); // vérification toutes les 10ms
-	document.addEventListener("keydown", keyboard); // on écoute les events du clavier
+	var interval = setInterval(jeu, 10); // vérification toutes les 10ms
+	document.addEventListener("keydown", clavier); // on écoute les events du clavier
 }
+
 function jeu() {
 	// le plus gros du taff mdr
+	contexte.fillStyle="f1c40f";
+	contexte.fillRect(x, y, largeur, hauteur);
 }
-function keyboard(evt) {
-	switch(evt.keyCode) {
+
+function clavier(action) {
+	switch(action.keyCode) {
 	case 37: // à faire - tourner à gauche
 	break;
 	case 38: // à faire - monter
@@ -17,6 +25,8 @@ function keyboard(evt) {
 	case 39: // à faire - tourner à droite
 	break;
 	case 40: // à faire - descendre
+	break;
+	case 32: // à faire - espace
 	break;
 	}
 }
