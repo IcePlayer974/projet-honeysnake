1b772d4224ecbaea7332bb0976e0d828b5156c68
John Doe <81570777+etil2jz@users.noreply.github.com>
2021-10-08 08:33:34 +0200

    Update jeu.js

--- js/jeu.js
+++ js/jeu.js
@@ -5,19 +5,33 @@ var x = Math.trunc(canvas.height / 2);
 var y = Math.trunc(canvas.width / 2);
 var depX = depY = 0;
 var trace = [];
-var tailleTrace = tailleInitTrace = 10;
+var tailleTrace = 10; // à changer plus tard
+var sautTrace = 2; // à changer plus tard
+var tailleMaxTrace = 1000; // à changer plus tard
+var hist;
+var compteBoucle = 0;
+var sautBoucle = 10;
 var i;
 
 // ça charge ce bout de code uniquement quand la page a entièrement chargé
 window.onload=function() {
 	var interval = setInterval(jeu, 500); // vérification toutes les 500ms
-	document.addEventListener("keydown", clavier); // on écoute les events du clavier
+	document.addEventListener("keydown", keyboard); // on écoute les events du keyboard
 }
 
 function jeu() {
 	// le plus gros du taff mdr
 	x += depX;
 	y += depY;
+	// on augmente tailleTrace toutes les secondes, 100 boucles
+	if ((tailleTrace <= tailleMaxTrace) && ((depX != 0) || (depY != 0))) {
+		if ((compteBoucle++)%100 == 1) {
+			sautBoucle--;
+			if (sautBoucle < 0) {
+				tailleTrace += sautTrace;
+			}
+		}
+	}
 	trace.push({x:x, y:y}); // on met x et y dans le tableau
 	while (trace.length>tailleTrace) { // tant que le tableau (la trace) dépasse la taille max
 		// on en enlève un
@@ -30,25 +44,38 @@ function jeu() {
 	}
 }
 
-function clavier(action) {
-	switch(action.keyCode) {
-	case 37: // à faire - tourner à gauche
+function keyboard(evt) {
+	switch(evt.keyCode) {
+	case 37:
+	// touche gauche
+	if (hist == 39) { break; }
+	hist = 37;
 	depX = -25;
 	depY = 0;
 	break;
-	case 38: // à faire - monter
+	case 38:
+	// touche haut
+	if (hist == 40) { break; }
+	hist = 38;
 	depX = 0;
 	depY = -25;
 	break;
-	case 39: // à faire - tourner à droite
+	case 39:
+	// touche droite
+	if (hist == 37) { break; }
+	hist = 39;
 	depX = 25;
 	depY = 0;
 	break;
-	case 40: // à faire - descendre
+	case 40:
+	// touche bas
+	if (hist == 38) { break; }
+	hist = 40;
 	depX = 0;
 	depY = 25;
 	break;
-	case 32: // à faire - espace
+	case 32:
+	// touche espace pour pause
 	depX = 0;
 	depY = 0;
 	break;
