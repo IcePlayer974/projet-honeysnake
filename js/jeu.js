a46c4cc83dd86831d4f90af2d4acd95cf38002da
IcePlayer974 <90893959+IcePlayer974@users.noreply.github.com>
2021-10-08 11:14:01 +0200

    Update jeu.js

--- js/jeu.js
+++ js/jeu.js
@@ -1,12 +1,12 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
-var grid = 32;
+var grid = 25;
 var count = 0;
   
 var snake = {
-  x: 320,
-  y: 320,
+  x: 275,
+  y: 275,
   
   // Déplacements du serpent, bouge d'une case dans la direction X ou Y
   dx: grid,
@@ -19,8 +19,8 @@ var snake = {
   maxCells: 4
 };
 var apple = {
-  x: 640,
-  y: 640
+  x: 425,
+  y: 425,
 };
 
 // get random whole numbers in a specific range
